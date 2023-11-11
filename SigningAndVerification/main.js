92cf9b4bcce88be1406f5de26a64c50e9aaaf78c
Amal Saju Kalarickal <amalsaju1998@gmail.com>
2023-11-11 23:43:56 +1100

    Added SHA256 signing and verification

--- SigningAndVerification/main.js
+++ SigningAndVerification/main.js
@@ -1,3 +1,6 @@
+
+//import { randomBytes } from 'ed25519-keygen/utils';
+//const ed25519 = require('@noble/ed25519');
 const express = require('express');
 const http = require('http');
 const forge = require('node-forge');
@@ -12,8 +15,9 @@ const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname)));
 
+// SHA256 Signing
 io.on('connection', (socket) => {
-  socket.on('sign and verify', (message, callback) => {
+  socket.on('sign and verify SHA256', (message, callback) => {
     const keys = forge.pki.rsa.generateKeyPair(2048);
     const privateKeyPem = forge.pki.privateKeyToPem(keys.privateKey);
     const sign = crypto.createSign('SHA256');
@@ -26,6 +30,11 @@ io.on('connection', (socket) => {
     callback(isVerified, privateKeyPem, signature, publicKeyPem);
   });
 });
+
+// Ed25519 Signing
+
+//ed25519.verify(skyes.signature, );
+
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
