2ec3cee08396e834b1165f03af55bc077663465e
Amal Saju Kalarickal <amalsaju1998@gmail.com>
2023-11-11 19:35:43 +1100

    Signing and Verifying using Public-Private keys using forge to create keys and crypto to sign and verify them.

---
+++ SigningAndVerification/main.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const forge = require('node-forge');
+const crypto = require('crypto');
+const socketIo = require('socket.io');
+
+
+const path = require('path');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname)));
+
+io.on('connection', (socket) => {
+  socket.on('sign and verify', (message, callback) => {
+    const keys = forge.pki.rsa.generateKeyPair(2048);
+    const privateKeyPem = forge.pki.privateKeyToPem(keys.privateKey);
+    const sign = crypto.createSign('SHA256');
+    sign.update(message);
+    const signature = sign.sign(privateKeyPem, 'hex');
+    const publicKeyPem = forge.pki.publicKeyToPem(keys.publicKey);
+    const verify = crypto.createVerify('SHA256');
+    verify.update(message);
+    const isVerified = verify.verify(publicKeyPem, signature, 'hex');
+    callback(isVerified, privateKeyPem, signature, publicKeyPem);
+  });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
